42ef0d7cac0adf320996e260c731dbfd10f5b8ec
Null <auguselo77@gmail.com>
2026-07-30 02:30:11 -0300

    bug /

--- js/Legal.js
+++ js/Legal.js
@@ -47,7 +47,7 @@ function ViewTermsOfService() {
 }
 
 async function Load_Legal_Content(name) {
-    fetch(`Laws/${Lang}/${name}.md`).then(res => {
+    fetch(`${window.location.pathname.split("/Legal/")[0]}Laws/${Lang}/${name}.md`).then(res => {
         if (res.status == 200) {
             return res.text();
         }
